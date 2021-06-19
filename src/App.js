4ff2474c3d1b56e32b1e6919d57cfe51edb652a6
claudinei casa <claudinei.casa@ufpr.br>
2021-06-19 00:50:06 -0300

    [add] integration with electron

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import { Notifier } from './Notifier.js'
 import { Timer } from "./Timer.js"
 import { Emitter } from './Emitter.js'
-const notify = Notifier.notify({
-    title: 'Hora do Post!!!',
-    body: 'Crie algo agora'
-})
+
+const message = [{
+        title: "ola",
+        body: "Crie algo agora"
+    },
+    {
+        title: "bom dia",
+        body: "Crie algo agora"
+    },
+    {
+        title: "que tal andar de bike um pouco?",
+        body: "Crie algo agora"
+    },
+    {
+        title: "bora codar?",
+        body: "Crie algo agora"
+    },
+]
+const notify = Notifier.notify(message[1])
 const App = {
     async start() {
         try {
             await Notifier.init()
-            Emitter.on('countdown-start', notify)
+            Emitter.on('countdown-end', notify)
             Emitter.on('countdown-end', Timer.init)
-            Timer.init(5)
+            Timer.init(20)
         } catch (err) {
             console.log(err.message)
         }
